78e72dea2b2dbdf41b7ebd3ef85d5bfcc601aabc
Minh Nguyen <minhnguyen.smu@gmail.com>
2012-10-29 18:09:03 +0800

    My first commit of own

---
+++ Java&Android24/src/partII/Name.java
@@ -0,0 +1,15 @@
+package partII;
+
+import java.util.*;
+
+public class Name {
+	public static void main(String[] args) {
+		String names[] = { "Lauren", "Audrina", "Heidi", "Whitney",
+		        "Stephanie", "Spencer", "Lisa", "Brody", "Frankie",
+		        "Holly", "Jordan", "Brian", "Jason" };
+		System.out.print("The original order:");
+		for(int i = 0; i < names.length; i++) {
+			//System.out.print(i + ": ")
+		}
+	}
+}
