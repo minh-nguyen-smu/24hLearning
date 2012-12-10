b3d46c25af314439ab08d1bf8e0ae68e7bc5510a
Kumi <minhnguyen.smu@gmail.com>
2012-12-09 18:22:21 -0800

    Hour 13

---
+++ Java&Android24/src/Hour13/SalutonFrame.java
@@ -0,0 +1,25 @@
+package Hour13;
+
+import javax.swing.*;
+
+public class SalutonFrame extends JFrame {
+	public SalutonFrame() {
+		super("Saluton mondon!");
+		setLookAndFeel();
+		setSize(350,100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	private void setLookAndFeel() {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception exc) {
+			//ignore error
+		}
+	}
+	
+	public static void main(String[] arguments) {
+		SalutonFrame sal = new SalutonFrame();
+	}
+}
